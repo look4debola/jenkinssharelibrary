d6b9cd10e51c2cf809fa0b298ac92d8c1757391d
look4debola <134791704+look4debola@users.noreply.github.com>
2023-07-03 16:11:45 -0400

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,1 +1,12 @@
-
+def call(string "stagename"){
+	if ("${stagename}" == "build"){
+	sh "mvn clean package"
+	}
+  else if ("${stagename}" == "codeanalysis"){
+  sh "mvn sonar:sonar"
+  }
+  else if ("${stagename}" == "upload"){
+  sh "mvn deploy"
+  }
+ 
+}
